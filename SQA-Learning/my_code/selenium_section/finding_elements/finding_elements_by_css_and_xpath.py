26545f7ca39811007ddfb737605c1b55a568a119
Denis Burlakov <denis055023@gmail.com>
2023-07-05 23:56:26 +0200

    update project

--- SQA-Learning/my_code/selenium_section/finding_elements/finding_elements_by_css_and_xpath.py
+++ SQA-Learning/my_code/selenium_section/finding_elements/finding_elements_by_css_and_xpath.py
@@ -3,10 +3,10 @@ from selenium.webdriver.common.by import By
 import time
 
 driver = webdriver.Chrome()
-driver.get('http://demostore.supersqa.com')
+driver.get('http://mystore.local/')
 
 card = driver.find_element(By.CSS_SELECTOR, '#site-navigation > div:nth-child(2) > ul > li.page_item.page-item-7 > a')
 card.click()
-time.sleep(3)
+time.sleep(7)
 driver.quit()
 
