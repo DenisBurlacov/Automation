4744ae4c4f72a4968a55c57abbe230f1759bf3c1
Denis Burlakov <denis055023@gmail.com>
2023-07-09 12:38:47 +0200

    added url to the test page

--- SQA-Learning/my_code/selenium_section/waits/implicit_wait.py
+++ SQA-Learning/my_code/selenium_section/waits/implicit_wait.py
@@ -1,3 +1,6 @@
 from selenium import webdriver
+import time
 
-drivers = webdriver.Chrome()
+driver = webdriver.Chrome()
+driver.get('file:///Users/den/PycharmProjects/Automation/SQA-Learning/my_code/selenium_section/waits/page_with_slow_image.html')
+time.sleep(4)
